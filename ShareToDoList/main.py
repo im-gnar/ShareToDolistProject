3f4f0f6fcda267eaa00d22bb94f4dcaa11dc3a02
JunHo-YH <u_innovation@naver.com>
2021-02-18 22:08:21 +0900

    bootstrap 입히기

--- ShareToDoList/main.py
+++ ShareToDoList/main.py
@@ -1,41 +1,44 @@
 from flask import *
 import datetime
 
-app = Flask("ToDO")
-db={'test':'1234'}
+app = Flask("ToDO", static_url_path='/static')  # static 폴더 참조
+db = {'test': '1234'}
 now = datetime.datetime.now()
+
 
 @app.route('/')
 def mainpage():
     nowDatetime = now.strftime('%Y-%m-%d %H:%M:%S')
-    return render_template("main_need_login.html", date = nowDatetime)
-
+    return render_template('main_need_login.html')
 # /user={userid}
+
+
 @app.route('/user')
 def mainpageUsing():
     return render_template("main_logout.html")
 
 
 # ID, PWD를 POST로 받아와서 DB 데이터와 대조
-@app.route('/login',methods=["post","get"])
+@app.route('/login', methods=["post", "get"])
 def loginpage():
     Error = None
     id = request.form.get('id')  # 초기값 = None
     pwd = request.form.get('pwd')
 
-    if id!=None and pwd!=None:
+    if id != None and pwd != None:
         # id not exist error
         if id not in db.keys():
             Error = "ID does not exist"
 
         # password diff error
-        elif db[id]!=pwd:
+        elif db[id] != pwd:
             Error = "Password does not match"
 
         # login success
         else:
             return render_template("main_logout.html")
-    return render_template("login.html",Error = Error)
+    return render_template("login.html", Error=Error)
+
 
 @app.route('/signin')
 def signinpage():
@@ -47,5 +50,5 @@ def signinpage():
 def todopage():
     return render_template("todolist.html")
 
-app.run(host="127.0.0.1")
 
+app.run(host="127.0.0.1", debug=True)
