119e9f0c222acc48f854ae7a9f3bacea28211168
JunHo-YH <u_innovation@naver.com>
2021-02-19 14:46:45 +0900

    id_check

--- ShareToDoList/main.py
+++ ShareToDoList/main.py
@@ -2,25 +2,28 @@ from flask import *
 import datetime
 
 app = Flask("ToDO")
-db={'test':'1234','test2':'5678'}
+db = {'test': '1234', 'test2': '5678'}
 nowDatetime = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-roomList = [{'title':'room1','id':"host1"},{'title':'room2','id':"host2"}]   # Query = select title from roomList
+roomList = [{'title': 'room1', 'id': "host1"}, {
+    'title': 'room2', 'id': "host2"}]  # Query = select title from roomList
+
 
 @app.route('/')
 def mainpage():
     # link = "/login"
     # link_tag = "Log In"
     # msg = "Please log in first"
-    login=False
-    if not roomList:   # 빈 리스트는 false
+    login = False
+    if not roomList:  # 빈 리스트는 false
         msg = "no room now. create first room!"
     # return render_template("main_need_login.html",
     #                        link = link, link_tag=link_tag,
     #                        date = nowDatetime,
     #                        roomList = roomList,
     #                        msg = msg)
-    return render_template("main_need_login.html",login = login,date = nowDatetime,
-                            roomList = roomList)
+    return render_template("main_need_login.html", login=login, date=nowDatetime,
+                           roomList=roomList)
+
 
 # /user={userid}
 @app.route('/user')
@@ -29,19 +32,19 @@ def mainpageUsing():
 
 
 # ID, PWD를 POST로 받아와서 DB 데이터와 대조
-@app.route('/login',methods=["post","get"])
+@app.route('/login', methods=["post", "get"])
 def loginpage():
     Error = None
     id = request.form.get('id')  # 초기값 = None
     pwd = request.form.get('pwd')
 
-    if id!=None and pwd!=None:
+    if id != None and pwd != None:
         # id not exist error
         if id not in db.keys():
             Error = "ID does not exist"
 
         # password diff error
-        elif db[id]!=pwd:
+        elif db[id] != pwd:
             Error = "Password does not match"
 
         # login success
@@ -50,17 +53,34 @@ def loginpage():
             # link_tag="Log Out"
             login = True
             return render_template("main_need_login.html",
-                                   date=nowDatetime, login = login,roomList = roomList)
-    return render_template("login.html",Error = Error)
+                                   date=nowDatetime, login=login, roomList=roomList)
+    return render_template("login.html", Error=Error)
+
 
 @app.route('/signin')
 def signinpage():
     return render_template("signin.html")
 
 
+f_db = {'sist': '1234'}
+
+
+@app.route('/signin', methods=["post", "get"])
+def id_check():
+    notify = None
+    id = request.form.get('id')
+    if id in f_db.keys():
+        notify = "다른 아이디를 사용해주세요"
+    else:
+        notify = "사용 가능한 아이디입니다."
+    return render_template("/signin.html", notify=notify)
+
 # {/{room list id}}
+
+
 @app.route('/')
 def todopage():
     return render_template("todolist.html")
+
 
 app.run(host="127.0.0.1")
