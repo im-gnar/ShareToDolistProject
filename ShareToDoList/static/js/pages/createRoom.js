e563ee696c00d0806d61b696490777906540fc9c
rachel5004 <75432228+rachel5004@users.noreply.github.com>
2021-02-25 15:27:38 +0900

    createRoom.js

---
+++ ShareToDoList/static/js/pages/createRoom.js
@@ -0,0 +1,40 @@
+const create = document.querySelector(".js-create"),
+    form = document.querySelector(".js-form");
+
+let createtitle = "origin";
+
+function wantCreate(event){
+    event.preventDefault();
+    const input = document.createElement("input");
+    input.placeholder = "input title";
+    form.appendChild(input);
+    create.disabled = true;
+    console.log(event);
+    form.addEventListener("submit",createRoom);
+}
+
+function createRoom(event){
+    event.preventDefault();
+    console.log(event);
+    const title = document.querySelector("input").value;
+    sendDB(title);
+    form.removeChild(document.querySelector("input"));
+    create.disabled = false;
+    // add to room list
+    // sendDB(currentValue);
+}
+
+function sendDB(text){
+    const newroom = {
+        title : text
+    };
+    console.log(newroom);
+//    host = localStorage.id?
+//    title = text
+}
+
+
+function init(){
+    create.addEventListener("click",wantCreate)
+}
+init();
